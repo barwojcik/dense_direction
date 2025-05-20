70a209a76e2d2644a8702db67692ebd63a42d87d
barwojcik <bw9870@gmail.com>
2025-05-20 15:04:12 +0200

    Added ability to calculate loss at lower resolutions

--- dense_direction/modeling/heads/direction_head.py
+++ dense_direction/modeling/heads/direction_head.py
@@ -6,7 +6,7 @@ of mmseg's BaseDecoderHead.
 """
 
 from abc import ABCMeta, abstractmethod
-from typing import List, Tuple
+from typing import Sequence
 
 import torch
 from torch import Tensor
@@ -85,6 +85,8 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
             vector filed resize. Default: False.
         post_norm_vectors (bool): Whether apply L2 normalization to directional vectors in output
             vector filed. Default: False.
+        gt_scale_factor (float): Per class sematic segmentation map resize factor for loss
+            calculation. Default: 1.
         init_cfg (dict or list[dict], optional): Initialization config dict.
     """
 
@@ -98,24 +100,93 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
 
     def __init__(
         self,
-        in_channels,
-        channels,
+        in_channels: int,
+        channels: int,
         *,
-        dir_classes=None,
-        dropout_ratio=0.1,
-        conv_cfg=None,
-        norm_cfg=None,
-        act_cfg=None,
-        in_index=-1,
-        input_transform=None,
-        loss_decode=None,
-        ignore_index=255,
-        sampler=None,
-        align_corners=False,
-        pre_norm_vectors=False,
-        post_norm_vectors=False,
-        init_cfg=None,
+        dir_classes: Sequence[int]=None,
+        dropout_ratio: float=0.1,
+        conv_cfg: ConfigType=None,
+        norm_cfg: ConfigType=None,
+        act_cfg: ConfigType=None,
+        in_index: int | Sequence[int]=-1,
+        input_transform: str=None,
+        loss_decode: ConfigType=None,
+        ignore_index: int=255,
+        sampler: ConfigType=None,
+        align_corners: bool=False,
+        pre_norm_vectors: bool=False,
+        post_norm_vectors: bool=False,
+        gt_scale_factor: float=1.,
+        init_cfg: ConfigType=None,
     ) -> None:
+        """
+        Base class for direction estimation decoder heads.
+
+        This class is a direction estimation counterpart of mmseg's BaseDecoderHead.
+
+        From BaseDecoderHead:
+
+        1. The ``init_weights`` method is used to initialize decode_head's
+        model parameters. After segmentor initialization, ``init_weights``
+        is triggered when ``segmentor.init_weights()`` is called externally.
+
+        2. The ``loss`` method is used to calculate the loss of decode_head,
+        which includes two steps: (1) the decode_head model performs forward
+        propagation to obtain the feature maps (2) The ``loss_by_feat`` method
+        is called based on the feature maps to calculate the loss.
+
+        .. code:: text
+
+        loss(): forward() -> loss_by_feat()
+
+        3. The ``predict`` method is used to predict segmentation results,
+        which includes two steps: (1) the decode_head model performs forward
+        propagation to obtain the feature maps (2) The ``predict_by_feat`` method
+        is called based on the feature maps to predict segmentation results
+        including post-processing.
+
+        .. code:: text
+
+        predict(): forward() -> predict_by_feat()
+
+        Args:
+            in_channels (int|Sequence[int]): Input channels.
+            channels (int): Channels after modules, before conv_dir.
+            dir_classes (int|Sequence[int]): Index or sequence of indexes of classes for which
+                direction estimation is performed. If not provided, it assumes binary segmentation and
+                positive class as linear. Default: None.
+            dropout_ratio (float): Ratio of dropout layer. Default: 0.1.
+            conv_cfg (dict|None): Config of conv layers. Default: None.
+            norm_cfg (dict|None): Config of norm layers. Default: None.
+            act_cfg (dict): Config of activation layers. Default: dict(type='ReLU').
+            in_index (int|Sequence[int]): Input feature index. Default: -1
+            input_transform (str|None): Transformation type of input features.
+                Options: 'resize_concat', 'multiple_select', None.
+                'resize_concat': Multiple feature maps will be resized to the same size as the first
+                    one and then concat together.
+                'multiple_select': Multiple feature maps will be bundled into a list and passed into
+                    decode head.
+                None: Only one select feature map is allowed.
+                Default: None.
+            loss_decode (dict | Sequence[dict]): Config of decode loss.
+                The `loss_name` is property of the corresponding loss function which could be shown
+                in the training log. If you want this loss item to be included in the backward graph,
+                `loss_` must be the prefix of the name. Defaults to 'loss_dir'.
+                e.g. dict(type='DirectionalLoss'),
+                [dict(type='DirectionalLoss', loss_name='loss_dir'),
+                dict(type='SmoothnessLoss', loss_name='loss_smt')]
+                Default: dict(type='DirectionalLoss').
+            sampler (dict|None): The config of segmentation map sampler.
+                Default: None.
+            align_corners (bool): align_corners argument of F.interpolate. Default: False.
+            pre_norm_vectors (bool): Whether apply L2 normalization to directional vectors before
+                vector filed resize. Default: False.
+            post_norm_vectors (bool): Whether apply L2 normalization to directional vectors in output
+                vector filed. Default: False.
+            gt_scale_factor (float): Per class sematic segmentation map resize factor for loss
+                calculation. Default: 1.
+            init_cfg (dict or list[dict], optional): Initialization config dict.
+        """
         super().__init__(init_cfg or self.DEFAULT_INIT_CFG)
         self._init_inputs(in_channels, in_index, input_transform)
         self.channels = channels
@@ -126,11 +197,11 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
         self.norm_cfg = norm_cfg
         self.act_cfg = act_cfg or self.DEFAULT_ACT_CFG
         self.in_index = in_index
-
         self.ignore_index = ignore_index
         self.align_corners = align_corners
         self.pre_norm_vectors = pre_norm_vectors
         self.post_norm_vectors = post_norm_vectors
+        self.gt_scale_factor = gt_scale_factor
 
         # 2 vector components per class
         self.out_channels = 2 * self.num_classes
@@ -246,7 +317,7 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
             output = F.normalize(output, p=2, dim=2)
         return output
 
-    def loss(self, inputs: Tuple[Tensor], batch_data_samples: SampleList,
+    def loss(self, inputs: tuple[Tensor], batch_data_samples: SampleList,
              train_cfg: ConfigType) -> dict:
         """
         Forward function for training.
@@ -264,14 +335,14 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
         losses = self.loss_by_feat(dir_vectors, batch_data_samples)
         return losses
 
-    def predict(self, inputs: Tuple[Tensor], batch_img_metas: List[dict],
+    def predict(self, inputs: tuple[Tensor], batch_img_metas: list[dict],
                 test_cfg: ConfigType) -> Tensor:
         """
         Forward function for prediction.
 
         Args:
-            inputs (Tuple[Tensor]): List of multi-level img features.
-            batch_img_metas (dict): List Image info where each dict may also contain: 'img_shape',
+            inputs (tuple[Tensor]): List of multi-level img features.
+            batch_img_metas (list[dict]): List Image info where each dict may also contain: 'img_shape',
                 'scale_factor', 'flip', 'img_path', 'ori_shape', and 'pad_shape'.
                 For details on the values of these keys see
                 `mmseg/datasets/pipelines/formatting.py:PackSegInputs`.
@@ -284,30 +355,55 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
 
         return self.predict_by_feat(dir_vectors, batch_img_metas)
 
-    def _trasnform_gt_sem_seg(self, gt_sem_seg: Tensor) -> Tensor:
+    def _transform_gt_sem_seg(self, gt_sem_seg: Tensor) -> Tensor:
         """
         Transforms gt_sem_seg maps into separate binary maps per class for which directions will
         be estimated.
 
         Args:
-            gt_sem_seg (Tensor): Ground truth semantic segmentation map of shape (C, H, W).
+            gt_sem_seg (Tensor): Ground truth semantic segmentation map of shape (N, C, H, W).
 
         Returns:
-            Tensor: Semantic segmentation map of shape (K, C, H, W) where K number of linear classes.
+            Tensor: Semantic segmentation map of shape (N, K, 1, H, W) where K number of linear
+                classes.
         """
         class_maps = []
         for class_index in self.dir_classes:
             class_map = torch.where(gt_sem_seg == class_index, 1, 0)
             class_maps.append(class_map)
-        return torch.stack(class_maps, dim=0)
+        return torch.stack(class_maps, dim=1).float()
+
+    def _resize_per_class_gt_sem_seg(self, per_class_map: Tensor) -> Tensor:
+        """
+        Transforms gt_sem_seg maps into separate binary maps per class for which directions will
+        be estimated.
+
+        Args:
+            per_class_map (Tensor): Per class ground truth semantic segmentation map of
+                shape (N, K, 1, H, W).
+
+        Returns:
+            Tensor: Resized per class ground truth semantic segmentation map of shape (N, K, 1,
+                gt_scale_factor * H, gt_scale_factor * W).
+        """
+        return resize(
+            input=per_class_map.squeeze(2),
+            scale_factor=self.gt_scale_factor,
+            mode='bilinear',
+            align_corners=self.align_corners
+        ).unsqueeze(2)
 
     def _stack_batch_gt(self, batch_data_samples: SampleList) -> Tensor:
         """Stacks sematic segmentation ground truth maps into one batch."""
-        gt_semantic_segs = [data_sample.gt_sem_seg.data for data_sample in batch_data_samples]
-        gt_semantic_segs = [
-            self._trasnform_gt_sem_seg(gt_sem_seg) for gt_sem_seg in gt_semantic_segs
-        ]
-        return torch.stack(gt_semantic_segs, dim=0)
+        gt_semantic_segs: list[Tensor] = [data_sample.gt_sem_seg.data for data_sample in batch_data_samples]
+        gt_sem_seg: Tensor = torch.stack(gt_semantic_segs, dim=0)
+
+        per_class_gt_sem_seg: Tensor = self._transform_gt_sem_seg(gt_sem_seg)
+
+        if self.gt_scale_factor != 1.0:
+            per_class_gt_sem_seg: Tensor = self._resize_per_class_gt_sem_seg(per_class_gt_sem_seg)
+
+        return per_class_gt_sem_seg
 
     def loss_by_feat(self, dir_vectors: Tensor,
                      batch_data_samples: SampleList) -> dict:
@@ -336,7 +432,6 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
             seg_weight = self.sampler.sample(dir_vectors, seg_label)
         else:
             seg_weight = None
-        seg_label = seg_label.squeeze(1)
 
         if not isinstance(self.loss_decode, nn.ModuleList):
             losses_decode = [self.loss_decode]
@@ -359,7 +454,7 @@ class BaseDirectionDecodeHead(BaseModule, metaclass=ABCMeta):
         return loss
 
     def predict_by_feat(self, dir_vectors: Tensor,
-                        batch_img_metas: List[dict]) -> Tensor:
+                        batch_img_metas: list[dict]) -> Tensor:
         """
         Transforms a batch of output dir_vectors to the input shape.
 
