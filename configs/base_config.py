b99a023a3735fdd31f7b57bcbe61ec8f27b17db8
barwojcik <bw9870@gmail.com>
2025-07-17 20:54:04 +0200

    Added dinov2+dpt config files

--- configs/base_config.py
+++ configs/base_config.py
@@ -2,3 +2,12 @@ work_dir = "./outputs"
 default_scope = "mmseg"
 env_cfg = dict(dist_cfg=dict(backend="nccl"))
 log_level = "INFO"
+log_processor = dict(
+    window_size=10,
+    by_epoch=False,
+    custom_cfg=None,
+    num_digits=4,
+)
+runner = dict(
+    type="Runner",
+)
