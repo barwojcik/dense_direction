559350888aac6137df39ed1002edb5d394603512
barwojcik <bw9870@gmail.com>
2025-07-17 21:07:44 +0200

    Added missing import

--- dense_direction/data/datasets/__init__.py
+++ dense_direction/data/datasets/__init__.py
@@ -3,5 +3,6 @@ Module containing datasets classes.
 """
 
 from .concrete_cracks import ConcreteCracksDataset
+from .ottawa_roads import OttawaRoadsDataset
 
-__all__ = [ConcreteCracksDataset.__name__]
+__all__ = [ConcreteCracksDataset.__name__, OttawaRoadsDataset.__name__]
